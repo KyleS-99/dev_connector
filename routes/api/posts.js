268e4764910f843a63506a23b92db2c73628d599
unknown <kylestauch99@gmail.com>
2018-06-11 16:33:19 -0500

    Add POST /api/posts/ with validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-// @route   GET /api/posts/test
-// @desc    Tests post route
-// @access  Public
-router.get('/test', (req, res) => {
-    res.json({ msg: 'Post works' });
+// Load post model
+const Post = require('../../models/Post');
+
+// Post validation
+const validatePostInput = require('../../validation/post');
+
+// @route   POST /api/posts
+// @desc    Create post
+// @access  Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    const { text, name, avatar } = req.body;
+    const newPost = new Post({
+        text,
+        name,
+        avatar,
+        user: req.user.id
+    });
+
+    newPost.save().then((post) => res.json(post));
 });
 
 module.exports = router;
