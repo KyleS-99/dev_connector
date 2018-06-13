adbc14859edee0a0ca5272b4979905b3b84fcc6b
unknown <kylestauch99@gmail.com>
2018-06-12 21:44:06 -0500

    Add Routes POST /comment/:id and DELETE /comment/:id/:comment_id with validation

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,7 +97,7 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
     Post.findById(req.params.id)
         .then((post) => {
             if (post.likes.filter((like) => like.user.toString() === req.user.id).length === 0) {
-                return res.status(400).json({ noliked: 'You\'ve not yet liked this post' })
+                return res.status(400).json({ noliked: 'You\'ve not yet liked this post' });
             }
 
             // remove user that liked the post from the post.likes array
@@ -107,6 +107,66 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
             post.save().then((post) => res.json(post));
         })
         .catch((e) => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+// @route   POST /api/posts/comment/:id
+// @desc    Add comment to post
+// @access  Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+        .then((post) => {
+            const { text, name, avatar } = req.body;
+            const newComment = {
+                text,
+                name,
+                avatar,
+                user: req.user.id
+            };
+
+            // Add to comments array
+            post.comments.unshift(newComment);
+
+            // Save to database
+            post.save().then((post) => res.json(post));
+        })
+        .catch(() => res.status(404).json({ postnotfound: 'No post found' }));
+});
+
+// @route   DELETE /api/posts/comment/:id/:comment_id
+// @desc    Delete a comment to post
+// @access  Private
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then((post) => {
+            // Check to see if comment exists
+            if (post.comments.filter((comment) => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res.status(404).json({ commentnotexists: 'Comment doesn\'t exist' });
+            }
+
+            // Filter the comment to be deleted
+            const updatedComments = post.comments.filter((comment) => !(comment.user.toString() === req.user.id && comment._id.toString() === req.params.comment_id));
+
+            // Check to see if the user is authorized to delete that comment
+            // They will only be able to delete that comment if they're the creator of it
+            if (updatedComments.length === post.comments.length) {
+                return res.status(401).json({ notauthorized: 'If you\'re seeing this that means either of three things \n 1. You used postman or some other tool to send this request to delete someone else\'s comment \n 2. You changed the JavaScript on the front-end to send this request \n 3. You made your own script to make the requst.'});
+            }
+
+            // Update comments
+            post.comments = updatedComments;
+
+            // Save to database
+            post.save().then((post) => res.json(post));
+        })
+        .catch(() => res.status(404).json({ postnotfound: 'No post found' }));
 });
 
 module.exports = router;
