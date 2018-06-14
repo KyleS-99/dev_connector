cc9176ce72135dfee4af7b440a16c66b795daaa0
unknown <kylestauch99@gmail.com>
2018-06-14 17:40:37 -0500

    Added login and register, and updated landing and navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Navbar />
-				<Landing />
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Route path="/" exact={true} component={Landing} />
+
+					<div className="container">
+						<Route exact={true} path="/register" component={Register} />
+						<Route exact={true} path="/login" component={Login} />
+					</div>
+
+					<Footer />
+				</div>
+			</BrowserRouter>
 		);
 	}
 }
