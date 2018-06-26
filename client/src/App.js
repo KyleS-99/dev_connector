d3a7c7d61571dc09928565db90faca4db3de4ff9
unknown <kylestauch99@gmail.com>
2018-06-26 17:23:11 -0500

    Created 4 new components, and setup getProfileByHandle, when component mounts make a request to the api, get user by their handle then set them in state

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Profile from './components/Profile/Profile';
 
 import './App.css';
 
@@ -59,21 +60,11 @@ class App extends Component {
 						/>
 						
 						<div className="container">
-							<Route 
-								exact={true} 
-								path="/register" 
-								component={Register} 
-							/>
-							<Route 
-								exact={true} 
-								path="/login" 
-								component={Login} 
-							/>
-							<Route 
-								exact={true} 
-								path="/profiles" 
-								component={Profiles} 
-							/>
+							<Route exact={true} path="/register" component={Register} />
+							<Route exact={true} path="/login" component={Login} />
+							<Route exact={true} path="/profiles" component={Profiles} />
+							<Route exact={true} path="/profile/:handle" component={Profile} />
+							
 							<Switch>
 								<PrivateRoute 
 									exact={true} 
---
+++ client/src/components/Profile/ProfileGithub.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProfileGithub extends Component {
+    render() {
+        return (
+            <div>
+                <h1>TODO: PROFILE GITHUB</h1>
+            </div>
+        );
+    }
+}
+
+export default ProfileGithub;
